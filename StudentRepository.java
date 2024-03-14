97ad41314be314ccfbb75b92329814a60bdd0e51
vinay12343 <130041451+vinay12343@users.noreply.github.com>
2024-03-14 11:29:53 +0530

    Add files via upload

---
+++ StudentRepository.java
@@ -0,0 +1,6 @@
+package dataManagement.dm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
